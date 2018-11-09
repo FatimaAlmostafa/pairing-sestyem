12de1ad8bb5bc39436dbddd880b1ea7ce6b0b6f6
FatimaAlmostafa <ff242468@gmail.com>
2018-11-09 11:22:46 +0200

    student controller

--- server/resources/Students/StudentsController.js
+++ server/resources/Students/StudentsController.js
@@ -1,10 +1,55 @@
 var Students = require('./Students');
 
 exports.create = function (req, res) {
-  
+  var studentname = req.body.studentname;
+ var studentlevel = req.body.studentlevel;
+
+
+var student = new db.StudentSchema ({
+    studentname,
+    studentlevel
+  })
+student.save (function (err, date) {
+    if (err){
+      console.log ("errr in creat student")
+    }
+    res.send ("created student sucssfully");
+  })
+
+}
+  /////////////////////////////////////////
+  exports.findStudent = function (req, res) {
+    db.StudentSchema.find({}, function (err, data){
+      if (err){
+        console.log("err in show all student");
+      }
+      res.send(data);
+    })
+  };
+
+//////// / ///////////////////////////////////////////////
+
+exports.editStudent = function (req, res) {
+  var id = req.body._id;
+  var editStudentlevel = req.body.studentlevel;
+  db.StudentSchema.findOneAndUpdate({_id:id}, {$set: {studentLevel}},
+    function (req, res){
+      if (err){
+        console.log("err in update data");
+      }
+      res.send(data);
+    }
+  )
+
+}
+//////////////////////////////////////////////////////////////////
+exports.deleteStudent = function (req,res){
+  var id = req.body._id;
+  db.studentSchema.findOneAndRemove({_id: id}, function (err,data) {
+    if (err){
+      console.log("err in deleting Student");
+    }
+    res.send("deleting student sucssfully");
+  })
 };
-
-
-
-
- 
+/////////////////////////////////////////////////////////////////
